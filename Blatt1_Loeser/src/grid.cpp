50ac4ed4d74ed80efd2fc553ba2d97860e8dc234
sebastian <sebastian@ubuntu>
2015-10-28 14:23:48 -0700

    Alle Methoden bis auf Interpolate sind implementiert. Muss noch getestet werden. Es wurde gar keine Bib verwendet selbst max von Hand ausgerechnet.

--- Blatt1_Loeser/src/grid.cpp
+++ Blatt1_Loeser/src/grid.cpp
@@ -1,7 +1,7 @@
 #include "grid.hpp"
 
 #include <iostream>
-//#include <cmath>
+
 using namespace std;
 
 Grid::Grid(const Geometry* geom) {
@@ -20,7 +20,7 @@ Grid::~Grid() {
 }
 
 void Grid::Initialize(const real_t& value) {
-	for (int i = 0; i<= _geom->Size()[0]*_geom->Size()[1];i++){
+	for (int i = 0; i< _geom->Size()[0]*_geom->Size()[1];i++){
 		_data[i] = value;
 	}
 }
@@ -50,39 +50,65 @@ real_t Grid::dy_l(const Iterator& it) const {
 }
 
 real_t Grid::dy_r(const Iterator& it) const {
-	(_data[it.Top()]-_data[it])/_geom->Mesh()[1];
+	return (_data[it.Top()]-_data[it])/_geom->Mesh()[1];
 }
 
 real_t Grid::dxx(const Iterator& it) const {
-	(_data[it.Right()]-2*_data[it]+_data[it.Left()])/_geom->Mesh()[0]/_geom->Mesh()[0];
+	return (_data[it.Right()]-2*_data[it]+_data[it.Left()])/_geom->Mesh()[0]/_geom->Mesh()[0];
 }
 
 real_t Grid::dyy(const Iterator& it) const {
-	(_data[it.Top()]-2*_data[it]+_data[it.Down()])/_geom->Mesh()[1]/_geom->Mesh()[1];
+	return (_data[it.Top()]-2*_data[it]+_data[it.Down()])/_geom->Mesh()[1]/_geom->Mesh()[1];
 }
 
 real_t Grid::DC_udu_x(const Iterator& it, const real_t& alpha) const {
+	return ( (_data[it]+_data[it.Right()])/2*(_data[it]+_data[it.Right()])/2 - (_data[it]+_data[it.Left()])/2*(_data[it]+_data[it.Left()])/2 )/_geom->Mesh()[0];
 }
 
 real_t Grid::DC_vdu_y(const Iterator& it, const real_t& alpha,
 		const Grid* v) const {
+	return ( (v->Cell(it)+v->Cell(it.Right()))/2*(_data[it]+_data[it.Top()])/2 - (v->Cell(it.Down())+ v->Cell(it.Down().Right()))/2 * (_data[it]+_data[it.Down()] )/2 )/_geom->Mesh()[1];
 }
 
 real_t Grid::DC_udv_x(const Iterator& it, const real_t& alpha,
 		const Grid* u) const {
+	return ( (u->Cell(it)+u->Cell(it.Top()))/2*(_data[it]+_data[it.Right()])/2 - (u->Cell(it.Left())+ u->Cell(it.Left().Top()))/2 * (_data[it]+_data[it.Left()] )/2 )/_geom->Mesh()[0];
+
 }
 
 real_t Grid::DC_vdv_y(const Iterator& it, const real_t& alpha) const {
+	return ( (_data[it]+_data[it.Top()])/2*(_data[it]+_data[it.Top()])/2 - (_data[it]+_data[it.Down()])/2*(_data[it]+_data[it.Down()])/2 )/_geom->Mesh()[1];
+
 }
 
 real_t Grid::Max() const {
+	real_t max = _data[0];
+	for (int i = 1; i< _geom->Size()[0]*_geom->Size()[1];i++){
+		if (_data[i] >= max)
+		max = _data[i];
+	}
+	return max;
 }
 
 real_t Grid::Min() const {
+	real_t min = _data[0];
+	for (int i = 1; i< _geom->Size()[0]*_geom->Size()[1];i++){
+		if (_data[i] <= min)
+		min = _data[i];
+	}
+	return min;
 }
 
 real_t Grid::AbsMax() const {
+	real_t max = this->Max();
+	real_t min = this->Min();
+	if ((max+min) > 0)
+		return max;
+	else
+		return min;
+
 }
 
 real_t* Grid::Data() {
+	return _data;
 }
