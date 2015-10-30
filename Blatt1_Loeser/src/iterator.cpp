fbe76ee8cd966a9e592bedde3aeb4988c5319f2d
MrHankey <mrhankey88@gmx.de>
2015-10-30 14:34:12 +0100

    -iterator iterator iterator

--- Blatt1_Loeser/src/iterator.cpp
+++ Blatt1_Loeser/src/iterator.cpp
@@ -84,7 +84,7 @@ void InteriorIterator::First() {
 }
 
 void InteriorIterator::Next() {
-	if ( (_value + 1 ) > (_geom->Size()[0]*(_geom->Size()[1] - 1)) - 1 )
+	if ( (_value + 1 ) >= (_geom->Size()[0]*(_geom->Size()[1] - 1)) - 1 )
 			_valid = false;
 	else if ( (_value + 2) % _geom->Size()[0] == 0 )
 		_value += 3;
