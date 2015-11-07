f2dc751090d31249e236a5624e2559259ab5af84
Ajax <Ajax@iReckon3>
2015-11-07 09:31:27 +0100

    Solver commenting

--- Blatt1_Loeser/src/solver.cpp
+++ Blatt1_Loeser/src/solver.cpp
@@ -4,6 +4,8 @@
 
 #include <cmath>
 
+/// Class Constuctor
+// @param geom  get field geometry
 Solver::Solver(const Geometry* geom) {
 	_geom = geom;
 }
@@ -11,11 +13,18 @@ Solver::Solver(const Geometry* geom) {
 Solver::~Solver() {
 }
 
+/// Calculate Residual
+// @param  it        calls iterator
+// @param  grid      get grid values
+// @return localRes  return step residual
 real_t Solver::localRes(const Iterator& it, const Grid* grid,
 		const Grid* rhs) const {
 	return fabs(grid->dxx(it) + grid->dyy(it) - rhs->Cell(it));
 }
 
+/// SOR Constructor
+// @param geom   get geometry
+// @param omega  get scaling factor
 SOR::SOR(const Geometry* geom, const real_t& omega) : Solver(geom) {
 	_geom  = geom;
 	_omega = omega;
@@ -24,9 +33,13 @@ SOR::SOR(const Geometry* geom, const real_t& omega) : Solver(geom) {
 SOR::~SOR() {
 }
 
+/// Implement SOR method
+// @param  grid   get grid values
+// @param  rhs    get right hand side of equation
+// @return Cycle  calculate all new p_ij for one cycle
 real_t SOR::Cycle(Grid* grid, const Grid* rhs) const {
+	// Initialize
 	InteriorIterator it = InteriorIterator(_geom);
-
 	real_t n, res, dx, dy, norm;
 
 	n    = 0;
@@ -35,11 +48,13 @@ real_t SOR::Cycle(Grid* grid, const Grid* rhs) const {
 	dy   = _geom->Mesh()[1];
 	norm = 0.5*( (dx*dx*dy*dy)/(dx*dx+dy*dy) );
 
+	// Cycle through all cells
 	while(it.Valid()) {
+		// Initialize
 		n++;
-
 		real_t pij, pij_d, pij_l, pij_t, pij_r, A, B, corr;
 
+		// define variables
 		pij  	= grid->Cell(it);
 		pij_d   = grid->Cell(it.Down());
 		pij_t 	= grid->Cell(it.Top());
@@ -51,12 +66,16 @@ real_t SOR::Cycle(Grid* grid, const Grid* rhs) const {
 
 		corr = A+B-rhs->Cell(it);
 
+		// Save new p_ij
 		grid->Cell(it)  = (1-_omega)*pij + _omega * norm * corr;
+		// Calculate and summ residual
 		real_t lRes = localRes(it,grid,rhs);
 		res += lRes;
 
+		// Next cell
 		it.Next();
-
 	}
+
+	// Norm residual
 	return res/n;
 }
