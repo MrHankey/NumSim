f838d531a8659d6e40bfa0280f45d65a95152ba4
MrHankey <mrhankey88@gmx.de>
2015-11-09 16:41:38 +0100

    -int -> index_t

--- Blatt1_Loeser/src/iterator.cpp
+++ Blatt1_Loeser/src/iterator.cpp
@@ -102,7 +102,7 @@ Iterator Iterator::Top() const {
 /// Returns an Iterator that is located below this one
 //  On bottom boundary iterator returns the same cell
 Iterator Iterator::Down() const {
-	int newIndex = _value - _geom->Size()[0];
+	index_t newIndex = _value - _geom->Size()[0];
 	if ( newIndex < 0 )
 		return Iterator(_geom, _value);
 	else
