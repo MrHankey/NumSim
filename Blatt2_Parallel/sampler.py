8be9e0c0c6abfa5896a98ed45a77edef526e7c41
MrHankey <mrhankey88@gmx.de>
2015-12-21 15:28:07 +0100

    added discrete option to sampler

--- Blatt2_Parallel/sampler.py
+++ Blatt2_Parallel/sampler.py
@@ -4,15 +4,25 @@ from time import sleep
 
 numProcs = int(sys.argv[1])
 numSamples = int(sys.argv[2])
+deterministic = bool(sys.argv[3])
 
 print "Starting " + str(numSamples) + " samples on " + str(numProcs) + " cores."
 
 procs = []
 
-args="./numsim"
+args= ["./numsim"]
+
+start = 1000.0
+end = 2000.0
+stepsize = (end-start)/(numSamples-1)
 
 for p in range (0, numProcs):
-    process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if deterministic:
+        process = subprocess.Popen(args + [str(start)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        start += stepsize
+    else:
+        process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
     procs.append(process)
     print "process sarted"
 
@@ -24,7 +34,11 @@ while (finished < numSamples):
         if proc.poll() is not None:
             finished += 1
             print "Processes finished: " + str(finished)
-            procs[index] = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            if deterministic:
+                procs[index] = subprocess.Popen(args + [str(start)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                start += stepsize
+            else:
+                procs[index] = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             
     sleep(0.050)
             
